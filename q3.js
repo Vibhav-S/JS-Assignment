16e66db3a35837d0b73673a58cd1cf5947c4521d
Vibhav-S <sharmavibhav65@gmail.com>
2026-04-08 11:21:25 +0530

    Update q3.js

--- q3.js
+++ q3.js
@@ -1,5 +1,4 @@
-// Question 3: Mirror Corridor
-// Find smallest X >= 0 such that N+X is palindrome and divisible by K
+
 
 var N = parseInt(prompt("Enter N:"));
 var K = parseInt(prompt("Enter K:"));
